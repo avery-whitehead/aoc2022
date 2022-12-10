b49080e22dd8b977bdeb921667d8833ca9df646e
Avery Whitehead <averywhthead@gmail.com>
2022-12-09 21:03:06 -0800

    Day 8 solution

--- p08/src/main.rs
+++ p08/src/main.rs
@@ -1,12 +1,9 @@
-use std::usize;
-
-#[derive(Debug)]
 struct Tree {
-    height: Option<u32>,
-    top: Option<u32>,
-    right: Option<u32>,
-    bottom: Option<u32>,
-    left: Option<u32>,
+    height: u32,
+    top: Vec<u32>,
+    right: Vec<u32>,
+    bottom: Vec<u32>,
+    left: Vec<u32>,
 }
 
 fn main() {
@@ -16,62 +13,101 @@ fn main() {
         .filter(|r| !r.is_empty())
         .collect::<Vec<&str>>();
     let trees = parse_trees(rows);
+    // Part 1
+    let visible_trees = trees.iter().fold(0, |acc, tree| {
+        if is_tree_edge(tree) || is_tree_visible(tree) {
+            return acc + 1;
+        }
+        acc
+    });
+    println!("{}", visible_trees);
+    // Part 2
+    let max_tree_score = trees.iter().map(calc_tree_score).max().unwrap_or_default();
+    println!("{}", max_tree_score);
 }
 
 fn parse_trees(tree_vec: Vec<&str>) -> Vec<Tree> {
     let mut trees = Vec::new();
     for (row_idx, row) in tree_vec.iter().enumerate() {
         for (col_idx, tree) in row.chars().enumerate() {
-            //println!("row: {}, col: {}, height: {}", row_idx, col_idx, tree);
             trees.push(Tree {
-                height: tree.to_digit(10),
-                top: get_tree_top(&tree_vec, row_idx, col_idx),
-                right: get_tree_right(&tree_vec, row_idx, col_idx),
-                bottom: get_tree_bottom(&tree_vec, row_idx, col_idx),
-                left: get_tree_left(&tree_vec, row_idx, col_idx),
+                height: tree.to_digit(10).unwrap_or_default(),
+                top: get_trees_top(&tree_vec, row_idx, col_idx).unwrap_or_default(),
+                right: get_trees_right(&tree_vec, row_idx, col_idx).unwrap_or_default(),
+                bottom: get_trees_bottom(&tree_vec, row_idx, col_idx).unwrap_or_default(),
+                left: get_trees_left(&tree_vec, row_idx, col_idx).unwrap_or_default(),
             });
         }
     }
     trees
 }
 
-fn get_tree_top(tree_vec: &Vec<&str>, row_idx: usize, col_idx: usize) -> Option<u32> {
-    tree_vec
-        .get(row_idx.checked_sub(1)?)?
-        .chars()
-        .nth(col_idx)?
-        .to_digit(10)
+fn get_trees_top(tree_vec: &Vec<&str>, row_idx: usize, col_idx: usize) -> Option<Vec<u32>> {
+    tree_vec.get(0..row_idx).map(|rows| {
+        rows.iter()
+            .rev()
+            .map(|row| row.chars().nth(col_idx)?.to_digit(10))
+            .flatten()
+            .collect()
+    })
 }
 
-fn get_tree_right(tree_vec: &Vec<&str>, row_idx: usize, col_idx: usize) -> Option<u32> {
-    tree_vec
-        .get(row_idx)?
-        .chars()
-        .nth(col_idx + 1)?
-        .to_digit(10)
+fn get_trees_right(tree_vec: &Vec<&str>, row_idx: usize, col_idx: usize) -> Option<Vec<u32>> {
+    tree_vec.get(row_idx).map(|row| {
+        row.chars()
+            .map(|c| c.to_digit(10))
+            .skip(col_idx + 1)
+            .flatten()
+            .collect()
+    })
 }
 
-fn get_tree_bottom(tree_vec: &Vec<&str>, row_idx: usize, col_idx: usize) -> Option<u32> {
-    tree_vec
-        .get(row_idx + 1)?
-        .chars()
-        .nth(col_idx)?
-        .to_digit(10)
+fn get_trees_bottom(tree_vec: &Vec<&str>, row_idx: usize, col_idx: usize) -> Option<Vec<u32>> {
+    tree_vec.get(row_idx + 1..tree_vec.len()).map(|rows| {
+        rows.iter()
+            .map(|row| row.chars().nth(col_idx)?.to_digit(10))
+            .flatten()
+            .collect()
+    })
 }
 
-fn get_tree_left(tree_vec: &Vec<&str>, row_idx: usize, col_idx: usize) -> Option<u32> {
-    tree_vec
-        .get(row_idx)?
-        .chars()
-        .nth(col_idx.checked_sub(1)?)?
-        .to_digit(10)
+fn get_trees_left(tree_vec: &Vec<&str>, row_idx: usize, col_idx: usize) -> Option<Vec<u32>> {
+    tree_vec.get(row_idx).map(|row| {
+        row.chars()
+            .rev()
+            .map(|c| c.to_digit(10))
+            .skip(row.len() - col_idx)
+            .flatten()
+            .collect()
+    })
 }
 
-fn print_trees(trees: Vec<Tree>) {
-    for tree in trees {
-        println!(
-            "HEIGHT: {:?}, TOP: {:?}, RIGHT: {:?}, BOTTOM: {:?}, LEFT: {:?}",
-            tree.height, tree.top, tree.right, tree.bottom, tree.left
-        );
+fn is_tree_edge(tree: &Tree) -> bool {
+    tree.top.is_empty() || tree.right.is_empty() || tree.bottom.is_empty() || tree.left.is_empty()
+}
+
+fn is_tree_visible(tree: &Tree) -> bool {
+    tree.top.iter().all(|h| &tree.height > h)
+        || tree.right.iter().all(|h| &tree.height > h)
+        || tree.bottom.iter().all(|h| &tree.height > h)
+        || tree.left.iter().all(|h| &tree.height > h)
+}
+
+fn calc_tree_score(tree: &Tree) -> usize {
+    let top_score = get_trees_until_height(&tree.height, &tree.top);
+    let right_score = get_trees_until_height(&tree.height, &tree.right);
+    let bottom_score = get_trees_until_height(&tree.height, &tree.bottom);
+    let left_score = get_trees_until_height(&tree.height, &tree.left);
+    top_score * right_score * bottom_score * left_score
+}
+
+fn get_trees_until_height(height: &u32, trees: &Vec<u32>) -> usize {
+    let mut count = 0;
+    for tree_height in trees {
+        count += 1;
+        if tree_height >= height {
+            break;
+        }
     }
+    count
 }
